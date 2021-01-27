4de208bd7e6abfec21ac1641546b80c7bac28828
Pablo <Pablo@LAPTOP-CEAP31CU>
2021-01-27 12:07:25 +0000

    Kata4 (version 2)

--- src/control/Kata4_IS2.java
+++ src/control/Kata4_IS2.java
@@ -5,7 +5,9 @@
  */
 package control;
 
+import java.util.List;
 import model.Histogram;
+import model.Mail;
 import view.HistogramDisplay;
 import view.MailHistogramBuilder;
 import view.MailListReader;
@@ -19,13 +21,35 @@ public class Kata4_IS2 {
     /**
      * @param args the command line arguments
      */
+    
+    private String fileName;
+    private List<Mail> mailList;
+    private Histogram<String> histogram;
+    private HistogramDisplay histoDisplay;
+    
     public static void main(String[] args) {
-        Histogram<String> histogram = MailHistogramBuilder.build(
-                MailListReader.read("mail.txt"));
-        
-        HistogramDisplay histoDisplay = new HistogramDisplay(
+        Kata4_IS2 kata = new Kata4_IS2();
+        kata.execute();
+    }
+    
+    private void execute() {
+        input();
+        process();
+        output();
+    }
+    
+    private void input() {
+        fileName = "mail.txt";
+        mailList =  MailListReader.read(fileName);
+    }
+    
+    private void process() {
+        histogram = MailHistogramBuilder.build(mailList);
+    }
+    
+    private void output() {
+        histoDisplay = new HistogramDisplay(
                 "Histograma Leyendo de Fichero", histogram);
-        
         histoDisplay.execute();
     }
     
